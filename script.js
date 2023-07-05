676504930442ba38f1c2fe81a55468ffd4c8171e
sahill01 <sahilravipandey786@gmail.com>
2023-07-06 01:08:47 +0530

    Added Loader

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
  
 "use strict";
 
+// Show the loader when the page starts loading
+document.addEventListener('DOMContentLoaded', function() {
+  var loader = document.getElementById('loader');
+  loader.style.display = 'block';
+});
+
+// Hide the loader when the page finishes loading
+window.addEventListener('load', function() {
+  var loader = document.getElementById('loader');
+  loader.style.display = 'none';
+});
+
 function qs(selector, all = false) {
   return all ? document.querySelectorAll(selector) : document.querySelector(selector);
 }
